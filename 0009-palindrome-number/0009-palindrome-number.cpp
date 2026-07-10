0904dd4b0844b4b6e5805a1366695de5294c4158
Arjit Raj <149428686+arjitraj42@users.noreply.github.com>
2026-07-10 22:16:10 +0530

    Time: 2 ms (50.89%), Space: 8.7 MB (22.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
+        int digits= 0;
+        int orignal = x;
+        long long rev = 0;
+
+        while(x > 0){
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        return orignal  == rev;
+    }
+};
