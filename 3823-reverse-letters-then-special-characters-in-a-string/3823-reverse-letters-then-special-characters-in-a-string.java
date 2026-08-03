bf279b4add5c6ffad07190d00063698fc3aa4320
Arjit Raj <149428686+arjitraj42@users.noreply.github.com>
2026-08-04 02:30:18 +0530

    Time: 7 ms (8.11%), Space: 47.5 MB (5.67%) - LeetHub

---
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String reverseByType(String s) {
+        String letter = "";
+        String symbol = "";
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetter(ch)){
+                letter += ch;
+            }else{
+                symbol += ch;
+            }
+        }
+        String ans = "";
+        int i = letter.length() - 1;
+        int j = symbol.length() - 1;
+
+        for(char ch : s.toCharArray()){
+            if(Character.isLetter(ch)){
+                ans += letter.charAt(i--);
+            }else{
+                ans += symbol.charAt(j--);
+            }
+        }
+        return ans;
+    }
+}
